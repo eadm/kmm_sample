146ed37c4c2455a11a907b185d0936a72a1bef42
Ruslan Davletshin <smartmorefeed@gmail.com>
2021-05-08 12:27:56 +0300

    setup iOS target

--- iosKMMAppTest/iosKMMAppTest/ContentView.swift
+++ iosKMMAppTest/iosKMMAppTest/ContentView.swift
@@ -2,10 +2,29 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-    let greet = Greeting().greeting()
+    let sampleFeature = SampleFeatureBuilder.init()
+        .build()
+    
+    @ObservedObject var stateStore: StateStore
     
     var body: some View {
-        Text(greet)
+        VStack(alignment: .leading) {
+            let counter = (self.stateStore.state as? SampleFeatureStateData)?.counter ?? 0
+            Text("\(counter)")
+            Button(action: {
+                self.buttonPressed()
+            }, label: {
+                Text("+")
+            })
+        }
+    }
+    
+    init() {
+        stateStore = StateStore(feature: sampleFeature)
+    }
+    
+    func buttonPressed() -> Void {
+        sampleFeature.onNewMessage(message: SampleFeatureMessageIncCounterClicked.init())
     }
 }
 
@@ -13,4 +32,15 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+class StateStore: ObservableObject {
+    init(feature: Presentation_reduxFeature) {
+        feature.addStateListener(listener: { newState in
+            self.state = newState as! SampleFeatureState
+        })
+    }
+    
+    @Published var state: SampleFeatureState =
+        SampleFeatureStateData(counter: 0)
 }
